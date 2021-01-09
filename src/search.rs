2ad842ca7b736d3a7cc371291619ee7a98009d19
primenumber <prime@kmc.gr.jp>
2021-01-09 17:59:33 +0900

    Add test for make_lookup_result

--- src/search.rs
+++ src/search.rs
@@ -457,6 +457,7 @@ async fn ybwc(
     (res, best, stat)
 }
 
+#[derive(PartialEq, Debug)]
 enum CacheLookupResult {
     Cut(i8),
     NoCut(i8, i8, u8)
@@ -478,6 +479,116 @@ fn make_lookup_result(res_cache: Option<ResCache>, alpha: &mut i8, beta: &mut i8
         }
     } else {
         CacheLookupResult::NoCut(lower, upper, old_best)
+    }
+}
+
+#[test]
+fn test_lookup_result() {
+    const TEST_BASE81: &str = "!#jiR;rO[ORNM2MN";
+    let board = Board::from_base81(TEST_BASE81).unwrap();
+    let res_cache = ResCache{
+        board,
+        lower: -24,
+        upper: 16,
+        gen: 3,
+        best: 0
+    };
+    // [alpha, beta] is contained in [lower, upper]
+    {
+        let mut alpha = -12;
+        let mut beta = 4;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::NoCut(res_cache.lower, res_cache.upper, res_cache.best));
+        assert_eq!(alpha, -12);
+        assert_eq!(beta, 4);
+    }
+    // [alpha, beta] contains [lower, upper]
+    {
+        let mut alpha = -30;
+        let mut beta = 20;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::NoCut(res_cache.lower, res_cache.upper, res_cache.best));
+        assert_eq!(alpha, res_cache.lower);
+        assert_eq!(beta, res_cache.upper);
+    }
+    // alpha < lower < beta < upper
+    {
+        let mut alpha = -32;
+        let mut beta = 8;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::NoCut(res_cache.lower, res_cache.upper, res_cache.best));
+        assert_eq!(alpha, res_cache.lower);
+        assert_eq!(beta, 8);
+    }
+    // lower < alpha < upper < beta
+    {
+        let mut alpha = -6;
+        let mut beta = 26;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::NoCut(res_cache.lower, res_cache.upper, res_cache.best));
+        assert_eq!(alpha, -6);
+        assert_eq!(beta, res_cache.upper);
+    }
+    // lower < upper < alpha < beta
+    {
+        let mut alpha = 22;
+        let mut beta = 46;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::Cut(res_cache.upper));
+        assert_eq!(alpha, 22);
+        assert_eq!(beta, res_cache.upper);
+    }
+    // alpha < beta < lower < upper
+    {
+        let mut alpha = -42;
+        let mut beta = -24;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::Cut(res_cache.lower));
+        assert_eq!(alpha, res_cache.lower);
+        assert_eq!(beta, -24);
+    }
+    // res_cache is None
+    {
+        let mut alpha = -6;
+        let mut beta = 26;
+        let result = make_lookup_result(None, &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::NoCut(-64, 64, PASS as u8));
+        assert_eq!(alpha, -6);
+        assert_eq!(beta, 26);
+    }
+    // lower < alpha = upper < beta
+    {
+        let mut alpha = 16;
+        let mut beta = 26;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::Cut(res_cache.upper));
+        assert_eq!(alpha, 16);
+        assert_eq!(beta, 16);
+    }
+    // alpha < beta = lower < upper
+    {
+        let mut alpha = -38;
+        let mut beta = -24;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::Cut(res_cache.lower));
+        assert_eq!(alpha, res_cache.lower);
+        assert_eq!(beta, -24);
+    }
+    // alpha < lower = upper < beta
+    {
+        let res_cache = ResCache{
+            board,
+            lower: 16,
+            upper: 16,
+            gen: 3,
+            best: 0
+        };
+        let mut alpha = -38;
+        let mut beta = 30;
+        let result = make_lookup_result(Some(res_cache.clone()), &mut alpha, &mut beta);
+        assert_eq!(result, CacheLookupResult::Cut(res_cache.lower));
+        assert_eq!(alpha, res_cache.lower);
+        assert_eq!(beta, res_cache.lower);
     }
 }
 
