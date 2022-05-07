1f0551ad20711feec8804c855d307145e3a3f43b
primenumber <prime@kmc.gr.jp>
2022-05-07 14:39:45 +0900

    Add codingame mode

--- src/main.rs
+++ src/main.rs
@@ -207,6 +207,123 @@ fn self_play(_matches: &ArgMatches) -> Board {
     board
 }
 
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+fn codingame(_matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
+    let search_params = SearchParams {
+        reduce: false,
+        ybwc_depth_limit: 12,
+        ybwc_elder_add: 1,
+        ybwc_younger_add: 2,
+        ybwc_empties_limit: 16,
+        eval_ordering_limit: 15,
+        res_cache_limit: 11,
+        stability_cut_limit: 12,
+        ffs_ordering_limit: 6,
+        static_ordering_limit: 3,
+    };
+    let evaluator = Arc::new(Evaluator::new("table-211122"));
+    let mut res_cache = ResCacheTable::new(256, 65536);
+    let mut eval_cache = EvalCacheTable::new(256, 65536);
+    let pool = ThreadPool::new().unwrap();
+    let mut reader = BufReader::new(std::io::stdin());
+
+    // read initial states
+    let id = {
+        let mut buf = String::new();
+        reader.read_line(&mut buf)?;
+        parse_input!(buf, usize)
+    };
+    let rows = {
+        let mut buf = String::new();
+        reader.read_line(&mut buf)?;
+        parse_input!(buf, usize)
+    };
+    let cols = rows;
+    loop {
+        // read board
+        let mut black = 0u64;
+        let mut white = 0u64;
+        for row in 0..rows {
+            let mut buf = String::new();
+            reader.read_line(&mut buf)?;
+            for (col, ch) in buf.chars().enumerate() {
+                match ch {
+                    '0' => black |= 1u64 << (row * cols + col),
+                    '1' => white |= 1u64 << (row * cols + col),
+                    _ => (),
+                }
+            }
+        }
+        let board = if id == 0 {
+            Board {
+                player: black,
+                opponent: white,
+                is_black: true,
+            }
+        } else {
+            Board {
+                player: white,
+                opponent: black,
+                is_black: false,
+            }
+        };
+        // read actions
+        let actions = {
+            let mut buf = String::new();
+            reader.read_line(&mut buf)?;
+            parse_input!(buf, usize)
+        };
+        for _ in 0..actions {
+            let mut buf = String::new();
+            reader.read_line(&mut buf)?;
+        }
+        // search
+        let best = if popcnt(board.empty()) > 16 {
+            let time_limit = 130;
+            let start = Instant::now();
+            let timer = Timer {
+                period: start,
+                time_limit,
+            };
+            let mut searcher = Searcher {
+                evaluator: evaluator.clone(),
+                cache: eval_cache.clone(),
+                timer: Some(timer),
+                node_count: 0,
+            };
+            let (score, best, depth) =
+                searcher.iterative_think(board, -64 * SCALE, 64 * SCALE, false);
+            eprintln!(
+                "Estimated result: {}, Depth: {}, Nodes: {}",
+                score, depth, searcher.node_count
+            );
+            best
+        } else {
+            let mut obj = SolveObj::new(
+                res_cache.clone(),
+                eval_cache.clone(),
+                evaluator.clone(),
+                search_params.clone(),
+                pool.clone(),
+            );
+            executor::block_on(solve_with_move(board, &mut obj))
+        };
+        eval_cache.inc_gen();
+        res_cache.inc_gen();
+        match best {
+            Hand::Play(pos) => {
+                println!("{}", pos_to_str(pos).to_ascii_lowercase());
+            }
+            _ => panic!(),
+        }
+    }
+}
+
 fn to_si(x: usize) -> String {
     if x == 0 {
         return "0".to_string();
@@ -639,6 +756,7 @@ fn main() {
                         .takes_value(true),
                 ),
         )
+        .subcommand(SubCommand::with_name("codingame").about("Codingame player"))
         .get_matches();
     match matches.subcommand() {
         ("ffobench", Some(_matches)) => {
@@ -691,6 +809,9 @@ fn main() {
         }
         ("eval-stats", Some(matches)) => {
             eval_stats(matches);
+        }
+        ("codingame", Some(matches)) => {
+            codingame(matches);
         }
         ("", None) => {
             eprintln!("Need subcommand");
