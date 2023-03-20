a9ae6da3f16184ad4aa7967844fcbe300ca025e6
primenumber <prime@kmc.gr.jp>
2023-03-20 09:14:32 +0900

    Moderate TT limit

--- src/main.rs
+++ src/main.rs
@@ -167,7 +167,7 @@ fn ffo_benchmark() {
         ybwc_younger_add: 2,
         ybwc_empties_limit: 18,
         eval_ordering_limit: 15,
-        res_cache_limit: 11,
+        res_cache_limit: 9,
         stability_cut_limit: 8,
         ffs_ordering_limit: 6,
         static_ordering_limit: 3,
