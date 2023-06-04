0d5dae203e80f2c60f5e58ee8ade7cc8905c302d
primenumber <prime@kmc.gr.jp>
2023-06-04 19:44:35 +0900

    Implement bench_eval

---
+++ src/engine/eval/test.rs
@@ -0,0 +1,39 @@
+extern crate test;
+use super::*;
+use crate::setup::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use test::Bencher;
+
+fn load_stress_test_set() -> Vec<(Board, i8)> {
+    let name = "problem/stress_test_54_1k.b81r";
+    let file = File::open(name).unwrap();
+    let reader = BufReader::new(file);
+    let mut dataset = Vec::new();
+    for (_idx, line) in reader.lines().enumerate() {
+        let line_str = line.unwrap();
+        let desired: i8 = line_str[17..].parse().unwrap();
+        match Board::from_base81(&line_str[..16]) {
+            Ok(board) => {
+                dataset.push((board, desired));
+            }
+            Err(_) => {
+                panic!();
+            }
+        }
+    }
+    dataset
+}
+
+#[bench]
+fn bench_eval(b: &mut Bencher) {
+    let solve_obj = setup_default();
+    let dataset = load_stress_test_set();
+
+    b.iter(|| {
+        dataset
+            .iter()
+            .map(|(board, _)| solve_obj.evaluator.eval(*board) as i32)
+            .sum::<i32>()
+    });
+}
