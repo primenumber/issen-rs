9afc90d26f0f918f9e044e15a1eb12cf4215ad41
primenumber <prime@kmc.gr.jp>
2023-11-28 08:50:33 +0900

    Fix node count

--- src/engine/midgame.rs
+++ src/engine/midgame.rs
@@ -235,35 +235,33 @@ struct ABDADAContext {
     solve_obj: SolveObj,
     cs_hash: Arc<Mutex<HashMap<Board, usize>>>,
     finished: Arc<AtomicBool>,
+    stats: SolveStat,
 }
 
 fn simplified_abdada_young(
     ctx: &mut ABDADAContext,
     (alpha, beta): (i8, i8),
-    stat: &mut SolveStat,
     deffered: &mut Vec<(Hand, Board)>,
     (pos, next): (Hand, Board),
     res: &mut i8,
     best: &mut Option<Hand>,
     depth: i8,
-) -> Option<(i8, Option<Hand>, SolveStat)> {
+) -> Option<(i8, Option<Hand>)> {
     if defer_search(next, &ctx.cs_hash) {
         deffered.push((pos, next));
         return None;
     }
     // NWS
     start_search(next, &ctx.cs_hash);
-    let (cres, _chand, cstat) = simplified_abdada_intro(ctx, next, (-alpha - 1, -alpha), false, depth + 1)?;
+    let (cres, _chand) = simplified_abdada_intro(ctx, next, (-alpha - 1, -alpha), false, depth + 1)?;
     finish_search(next, &ctx.cs_hash);
-    stat.merge(cstat);
     let mut tmp = -cres;
     if alpha < tmp && tmp < beta {
-        let (cres, _chand, cstat) = simplified_abdada_intro(ctx, next, (-beta, -tmp), false, depth + 1)?;
-        stat.merge(cstat);
+        let (cres, _chand) = simplified_abdada_intro(ctx, next, (-beta, -tmp), false, depth + 1)?;
         tmp = -cres;
     }
     if tmp >= beta {
-        return Some((tmp, Some(pos), *stat));
+        return Some((tmp, Some(pos)));
     }
     if tmp > *res {
         *best = Some(pos);
@@ -278,39 +276,34 @@ fn simplified_abdada_body(
     (mut alpha, beta): (i8, i8),
     passed: bool,
     depth: i8,
-) -> Option<(i8, Option<Hand>, SolveStat)> {
+) -> Option<(i8, Option<Hand>)> {
     let v = move_ordering_impl(&mut ctx.solve_obj, board, None);
-    let mut stat = SolveStat::one();
     if v.is_empty() {
         if passed {
-            return Some((board.score(), Some(Hand::Pass), stat));
+            return Some((board.score(), Some(Hand::Pass)));
         } else {
-            let (child_res, _child_best, child_stat) =
+            let (child_res, _child_best) =
                 simplified_abdada_intro(ctx, board.pass_unchecked(), (-beta, -alpha), true, depth)?;
-            stat.merge(child_stat);
-            return Some((-child_res, Some(Hand::Pass), stat));
+            return Some((-child_res, Some(Hand::Pass)));
         }
     }
     let mut res = -(BOARD_SIZE as i8);
     let mut best = None;
-    let mut stat = SolveStat::one();
     let mut deffered = Vec::new();
     for (i, (pos, next)) in v.into_iter().enumerate() {
         if i == 0 {
-            let (cres, _chand, cstat) = simplified_abdada_intro(ctx, next, (-beta, -alpha), false, depth + 1)?;
-            stat.merge(cstat);
+            let (cres, _chand) = simplified_abdada_intro(ctx, next, (-beta, -alpha), false, depth + 1)?;
             res = -cres;
             best = Some(pos);
             alpha = max(alpha, res);
             if alpha >= beta {
-                return Some((res, best, stat));
+                return Some((res, best));
             }
             continue;
         }
         if let Some(ret) = simplified_abdada_young(
             ctx,
             (alpha, beta),
-            &mut stat,
             &mut deffered,
             (pos, next),
             &mut res,
@@ -322,23 +315,21 @@ fn simplified_abdada_body(
     }
     for (pos, next) in deffered {
         // NWS
-        let (cres, _chand, cstat) = simplified_abdada_intro(ctx, next, (-alpha - 1, -alpha), false, depth + 1)?;
-        stat.merge(cstat);
+        let (cres, _chand) = simplified_abdada_intro(ctx, next, (-alpha - 1, -alpha), false, depth + 1)?;
         let mut tmp = -cres;
         if alpha < tmp && tmp < beta {
-            let (cres, _chand, cstat) = simplified_abdada_intro(ctx, next, (-beta, -tmp), false, depth + 1)?;
-            stat.merge(cstat);
+            let (cres, _chand) = simplified_abdada_intro(ctx, next, (-beta, -tmp), false, depth + 1)?;
             tmp = -cres;
         }
         if tmp >= beta {
-            return Some((tmp, Some(pos), stat));
+            return Some((tmp, Some(pos)));
         }
         if tmp > res {
             best = Some(pos);
             res = tmp;
         }
     }
-    Some((res, best, stat))
+    Some((res, best))
 }
 
 fn simplified_abdada_intro(
@@ -347,25 +338,27 @@ fn simplified_abdada_intro(
     (mut alpha, mut beta): (i8, i8),
     passed: bool,
     depth: i8,
-) -> Option<(i8, Option<Hand>, SolveStat)> {
+) -> Option<(i8, Option<Hand>)> {
     let rem = popcnt(board.empty());
     if depth >= ctx.solve_obj.params.ybwc_depth_limit || rem < ctx.solve_obj.params.ybwc_empties_limit {
         let (res, stat) = solve_inner(&mut ctx.solve_obj, board, (alpha, beta), passed);
-        return Some((res, None, stat));
+        ctx.stats.merge(stat);
+        return Some((res, None));
     }
+    ctx.stats.merge(SolveStat::one());
     if ctx.finished.load(std::sync::atomic::Ordering::SeqCst) {
         return None;
     }
     match stability_cut(board, (alpha, beta)) {
         CutType::NoCut => (),
-        CutType::MoreThanBeta(v) => return Some((v, None, SolveStat::one_stcut())),
-        CutType::LessThanAlpha(v) => return Some((v, None, SolveStat::one_stcut())),
+        CutType::MoreThanBeta(v) => return Some((v, None)),
+        CutType::LessThanAlpha(v) => return Some((v, None)),
     }
     let (lower, upper, _old_best) = match lookup_table(&mut ctx.solve_obj, board, (&mut alpha, &mut beta)) {
-        CacheLookupResult::Cut(v) => return Some((v, None, SolveStat::zero())),
+        CacheLookupResult::Cut(v) => return Some((v, None)),
         CacheLookupResult::NoCut(l, u, b) => (l, u, b),
     };
-    let (res, best, stat) = simplified_abdada_body(ctx, board, (alpha, beta), passed, depth)?;
+    let (res, best) = simplified_abdada_body(ctx, board, (alpha, beta), passed, depth)?;
     if rem >= ctx.solve_obj.params.res_cache_limit {
         update_table(
             ctx.solve_obj.res_cache.clone(),
@@ -377,7 +370,7 @@ fn simplified_abdada_intro(
             (lower, upper),
         );
     }
-    Some((res, best, stat))
+    Some((res, best))
 }
 
 fn start_search(board: Board, cs_hash: &Arc<Mutex<HashMap<Board, usize>>>) {
@@ -426,28 +419,25 @@ pub fn simplified_abdada(
             let finished = finished.clone();
             let finished2 = finished.clone();
             handles.push(s.spawn(move || {
-                let res = simplified_abdada_intro(
-                    &mut ABDADAContext {
-                        solve_obj,
-                        cs_hash,
-                        finished,
-                    },
-                    board,
-                    (alpha, beta),
-                    passed,
-                    depth,
-                );
+                let mut ctx = ABDADAContext {
+                    solve_obj,
+                    cs_hash,
+                    finished,
+                    stats: SolveStat::zero(),
+                };
+                let res = simplified_abdada_intro(&mut ctx, board, (alpha, beta), passed, depth);
                 finished2.store(true, std::sync::atomic::Ordering::SeqCst);
-                res
+                (res, ctx.stats)
             }));
         }
         let mut stat = SolveStat::zero();
         let mut res = -(BOARD_SIZE as i8);
         let mut best = None;
         for h in handles {
-            if let Some((tres, tbest, tstat)) = h.join().unwrap() {
-                stat.merge(tstat);
-                res = tres;
+            let (tres, tstat) = h.join().unwrap();
+            stat.merge(tstat);
+            if let Some((tscore, tbest)) = tres {
+                res = tscore;
                 best = tbest;
             }
         }
